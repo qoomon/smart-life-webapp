b18077ad29bce6e82f5ca1b7cc992be9281a106b
Bengt Brodersen <bengt.brodersen@gmail.com>
2021-05-25 13:28:24 +0200

    refactor: make use of vue script setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="nav">
-    <el-form v-if="!loginState" :model="loginForm" ref="form" :inline="true">
+    <el-form v-if="!loginState" :model="loginForm" :inline="true">
       <el-form-item label="Email address" size="medium">
         <el-input v-model="loginForm.username"></el-input>
       </el-form-item>
@@ -42,96 +42,99 @@
     </div>
   </div>
 </template>
-
 <script>
+export default {
+  name: 'Home'
+}
+</script>
+<script setup="" >
+/* eslint-disable no-unused-vars */
+import { ref, reactive, computed, onMounted } from 'vue'
+import { ElMessage } from "element-plus"
+
 import tuya from '@/libs/tuya'
 
 const homeAssistantClient = new tuya.HomeAssistantClient(
   JSON.parse(localStorage.getItem('session'))
 )
 
-export default {
-  name: 'Home',
-  components: {},
-  data () {
-    return {
-      loginState: false,
-      loginForm: {
-        username: '',
-        password: ''
-      },
-      devices: []
-    }
-  },
-  computed: {
-    devicesSorted () {
-      return [...this.devices].sort((d1, d2) => {
-        const v1 = d1.data.online === false ? 1 : 0
-        const v2 = d2.data.online === false ? 1 : 0
-        return v1 > v2 ? 1 : -1
-      })
-    }
-  },
-  methods: {
-    async login () {
-      await homeAssistantClient.login(
-        this.loginForm.username,
-        this.loginForm.password
-      )
-      localStorage.setItem('session', JSON.stringify(homeAssistantClient.getSession()))
-      this.loginState = true
-      await this.refreshDevices()
-    },
-    async logout () {
-      homeAssistantClient.dropSession()
-      localStorage.clear()
-      this.loginState = false
-      this.devices = []
-    },
-    async refreshDevices () {
-      // TODO handle expired session
-      // get device list
-      const discoveryResponse = await homeAssistantClient.deviceDiscovery()
-      const discoveryCode = discoveryResponse.header.code
-      if (discoveryCode === 'SUCCESS') {
-        this.devices = discoveryResponse.payload.devices
-        localStorage.setItem('devices', JSON.stringify(this.devices))
-      } else {
-        this.$message.error(`Oops, device discovery error. (${discoveryCode})`)
-      }
-    },
-    async toggleDevice (device) {
-      // TODO handle expired session
-      // TODO change icon to el-icon-loading
-      const newState = !device.data.state
-      const controlResponse = await homeAssistantClient.deviceControl(device.id, 'turnOnOff', newState)
-      const controlCode = controlResponse.header.code
-      if (controlCode === 'SUCCESS') {
-        device.data.state = newState
-      } else {
-        this.$message.error(`Oops, device control error. (${controlCode})`)
-      }
-    },
-    async triggerScene (device) {
-      // TODO handle expired session
-      // TODO change icon to el-icon-loading
-      const controlResponse = await homeAssistantClient.deviceControl(device.id, 'turnOnOff', true)
-      const controlCode = controlResponse.header.code
-      if (controlCode === 'SUCCESS') {
-        // do nothing
-        // todo user feedback
-      } else {
-        this.$message.error(`Oops, device control error. (${controlCode})`)
-      }
-    }
-  },
-  async mounted () {
-    // TODO handle expired session
-    this.loginState = !!homeAssistantClient.getSession()
-    if (!this.loginState) {
-      localStorage.clear()
-    }
-    this.devices = JSON.parse(localStorage.getItem('devices')) || []
+const loginState = ref(false)
+const devices = ref([])
+
+const devicesSorted = computed(() => {
+  const order = { true: 0, undefined: 1, false: 2 }
+  return devices.value.slice().sort((d1, d2) =>
+    order[d1.data.online] > order[d2.data.online] ? 1 : -1
+  )
+})
+
+const loginForm = ref({ username: '', password: '' })
+
+onMounted(async () => {
+  // TODO handle expired session
+  loginState.value = !!homeAssistantClient.getSession()
+  if (!loginState.value) {
+    localStorage.clear()
+  }
+  devices.value = JSON.parse(localStorage.getItem('devices')) || []
+})
+
+const login = async () => {
+  await homeAssistantClient.login(
+    loginForm.value.username,
+    loginForm.value.password
+  )
+  localStorage.setItem('session', JSON.stringify(homeAssistantClient.getSession()))
+  loginState.value = true
+  loginForm.value = { username: '', password: '' }
+  await refreshDevices()
+}
+
+const logout = () => {
+  homeAssistantClient.dropSession()
+  localStorage.clear()
+  loginState.value = false
+  loginForm.value = { username: '', password: '' }
+  devices.value = []
+}
+
+const refreshDevices = async () => {
+  // TODO handle expired session
+  // get device list
+  const discoveryResponse = await homeAssistantClient.deviceDiscovery()
+  const discoveryCode = discoveryResponse.header.code
+  if (discoveryCode === 'SUCCESS') {
+    const discoveryDevices = discoveryResponse.payload.devices || []
+    devices.value = discoveryDevices
+    localStorage.setItem('devices', JSON.stringify(discoveryDevices))
+  } else {
+    ElMessage.error(`Oops, device discovery error. (${discoveryCode})`)
+  }
+}
+
+const toggleDevice = async (device) => {
+  // TODO handle expired session
+  // TODO change icon to el-icon-loading
+  const newState = !device.data.state
+  const controlResponse = await homeAssistantClient.deviceControl(device.id, 'turnOnOff', newState)
+  const controlCode = controlResponse.header.code
+  if (controlCode === 'SUCCESS') {
+    device.data.state = newState
+  } else {
+    ElMessage.error(`Oops, device control error. (${controlCode})`)
+  }
+}
+
+const triggerScene = async (device) => {
+  // TODO handle expired session
+  // TODO change icon to el-icon-loading
+  const controlResponse = await homeAssistantClient.deviceControl(device.id, 'turnOnOff', true)
+  const controlCode = controlResponse.header.code
+  if (controlCode === 'SUCCESS') {
+    // do nothing
+    // todo user feedback
+  } else {
+    ElMessage.error(`Oops, device control error. (${controlCode})`)
   }
 }
 </script>
